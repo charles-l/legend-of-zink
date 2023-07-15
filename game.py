ad2b7d925350437c12e5c225aaeb96a4fd2393d8
nc <charles.saternos@gmail.com>
2023-07-15 09:26:18 -0400

    agario impl

--- game.py
+++ game.py
@@ -1,80 +1,98 @@
 # jousting
 from dataclasses import dataclass
+import random
 import pyray as rl
 import glm
 
 @dataclass
 class Player:
-    rect: rl.Rectangle
-    vel: glm.vec2
+    pos: glm.vec2
+    size: float
 
-players = [
-    Player(
-        rl.Rectangle(1, 1, 30, 80),
-        glm.vec2(0, 0)),
-    Player(
-        rl.Rectangle(100, 1, 30, 80),
-        glm.vec2(0, 0)),
-    ]
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 2400, 1000
+
+state = {
+    'players': [Player(glm.vec2(0, 0), 20), Player(glm.vec2(400, 10), 10)],
+    'pellets': [],
+}
 
 rl.init_window(WIDTH, HEIGHT, "game")
 rl.set_target_fps(60)
+
+PELLET_SIZE = 4
+
+# spawn pellets
+for i in range(100):
+    state['pellets'].append((random.randint(0, WIDTH), random.randint(0, HEIGHT)))
+
+@dataclass
+class Spring:
+    k: float
+    damping: float
+    value: float
+    v: float = 0
+
+    def update(self, target_value, dt=None):
+        springforce = -self.k*(self.value-target_value)
+        damp = self.damping * self.v
+        force = springforce - damp
+        if dt is None:
+            dt = rl.get_frame_time()
+        self.v += force * dt
+        self.value += self.v * dt
+        return self.value
+
+size_spring = Spring(70, 4, 0)
+
+# set up camera
+camera = rl.Camera2D()
+camera.offset = WIDTH / 2, HEIGHT / 2
+camera.zoom = 2
 while not rl.window_should_close():
-    players_input = [
-        glm.vec2(
-            -int(rl.is_key_down(rl.KEY_LEFT)) + int(rl.is_key_down(rl.KEY_RIGHT)),
-            -int(rl.is_key_down(rl.KEY_UP)),
-            ),
-        glm.vec2(
-            -int(rl.is_key_down(rl.KEY_A)) + int(rl.is_key_down(rl.KEY_D)),
-            -int(rl.is_key_down(rl.KEY_W)),
-            )
-        ]
+    player = state['players'][0]
+    mouse_pos = rl.get_screen_to_world_2d(rl.get_mouse_position(), camera)
+    move_diff = glm.vec2(mouse_pos.x, mouse_pos.y) - player.pos
+    speed = 5
+    diff_len = glm.length(move_diff)
+    if diff_len > speed:
+        move_diff /= glm.length(diff_len)
+        move_diff *= speed
+    player.pos += move_diff
 
-    ### input/logic
-    start_pos = [glm.vec2(player.rect.x, player.rect.y) for player in players]
-    for i, player in enumerate(players):
-        if player.rect.y + player.rect.height < HEIGHT:
-            player.vel.y += 1
-        else:
-            if players_input[i].y < 0:
-                player.vel.y = -13
+    if glm.distance((camera.target.x, camera.target.y), player.pos) > 100:
+        g = player.pos + glm.normalize((camera.target.x, camera.target.y) - player.pos) * 100
+        camera.target = (int(g[0]), int(g[1]))
 
-        if players_input[i].x != 0:
-            player.vel.x = players_input[i].x * 5
-        else:
-            player.vel.x = 0
+    pellets_to_delete = set()
+    for i, (px, py) in enumerate(state['pellets']):
+        if glm.distance((px, py), player.pos) < PELLET_SIZE + player.size:
+            player.size += 0.5
+            size_spring.value += 5
+            pellets_to_delete.add(i)
+    state['pellets'] = [x for i, x in enumerate(state['pellets']) if i not in pellets_to_delete]
 
-        player.rect.x += player.vel.x
-        player.rect.y += player.vel.y
-
-    for iter in range(3):
-        for i, player in enumerate(players):
-            for j, other_player in enumerate(players):
-                if i == j:
-                    continue
-                if rl.check_collision_recs(player.rect, other_player.rect):
-                    r = rl.get_collision_rec(player.rect, other_player.rect)
-                    print(r.width, r.height)
-                    if r.width < r.height:
-                        print('width')
-                        player.rect.x -= r.width / 2
-                        other_player.rect.x += r.width / 2
-                    else:
-                        print('height')
-                        player.rect.y -= r.height / 2
-                        other_player.rect.y += r.height / 2
-
-                player.rect.x = glm.clamp(player.rect.x, 0, WIDTH - player.rect.width)
-                player.rect.y = glm.clamp(player.rect.y, 0, HEIGHT - player.rect.height)
-
-    for i, player in enumerate(players):
-        player.vel = glm.vec2(player.rect.x, player.rect.y) - start_pos[i]
+    # handle eating other players
+    for i, p in enumerate(state['players']):
+        if p == player:
+            continue
+        p.pos += glm.normalize((random.choice([-1, 1]) * 5, random.choice([-1, 1]) * 5))
+        if glm.distance(p.pos, player.pos) < player.size + p.size:
+            if player.size < p.size:
+                state['players'][0].size = 0
+            else:
+                state['players'][i].size = 0
 
     ### drawing
     rl.begin_drawing()
-    rl.clear_background(rl.GRAY)
-    for player in players:
-        rl.draw_rectangle(int(player.rect.x), int(player.rect.y), int(player.rect.width), int(player.rect.height), rl.GREEN)
+    rl.begin_mode_2d(camera)
+    rl.clear_background(rl.LIGHTGRAY)
+    for px, py in state['pellets']:
+        rl.draw_circle(px, py, PELLET_SIZE, rl.BLUE)
+    for p in state['players']:
+        if p == player:
+            rl.draw_circle(int(p.pos.x), int(p.pos.y), size_spring.update(p.size), rl.GREEN)
+            rl.draw_circle_lines(int(p.pos.x), int(p.pos.y), p.size, rl.DARKGREEN)
+        else:
+            rl.draw_circle(int(p.pos.x), int(p.pos.y), p.size, rl.RED)
+    rl.end_mode_2d()
     rl.end_drawing()
