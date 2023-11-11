d01084846807c75f99c171845b37a5d4d01bc568
nc <charles.saternos@gmail.com>
2023-11-11 17:07:03 -0500

    checkpoint: clamped input

--- game.py
+++ game.py
@@ -8,7 +8,21 @@ state = SimpleNamespace(player=glm.vec2(10, 10))
 
 
 def update(state):
-    state.player.x += 1
+    input = glm.vec2()
+    if rl.is_key_down(rl.KEY_DOWN): input.y += 1
+    if rl.is_key_down(rl.KEY_UP): input.y -= 1
+    if rl.is_key_down(rl.KEY_LEFT): input.x -= 1
+    if rl.is_key_down(rl.KEY_RIGHT): input.x += 1
+
+    input.x += rl.get_gamepad_axis_movement(0, rl.GAMEPAD_AXIS_LEFT_X)
+    input.y += rl.get_gamepad_axis_movement(0, rl.GAMEPAD_AXIS_LEFT_Y)
+
+    util.debug_draw_input_axis(input)
+
+    if (vec_length := glm.length(input)) > 1:
+        input /= vec_length
+
+    state.player += input
 
 
 def draw(state):
