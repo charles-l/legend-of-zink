7317955abf54693e535a4bc99807e07a6be7b90c
nc <charles.saternos@gmail.com>
2023-11-11 17:00:15 -0500

    checkpoint: moving rectangle

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 import pyray as rl, glm, util
+from types import SimpleNamespace
 
 WIDTH, HEIGHT = 800, 600
 rl.init_window(WIDTH, HEIGHT, "My awesome game")
 
-state = None
+state = SimpleNamespace(player=glm.vec2(10, 10))
 
 
 def update(state):
-    ...
+    state.player.x += 1
 
 
 def draw(state):
-    ...
+    rl.clear_background(rl.BLACK)
+    rl.draw_rectangle(int(state.player.x), int(state.player.y), 100, 100, rl.RED)
 
 
+rl.set_target_fps(60)
 while not rl.window_should_close():
+    update(state)
     rl.begin_drawing()
+    rl.draw_fps(10, 10)
+    draw(state)
     rl.end_drawing()
