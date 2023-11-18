f8860409497557596fca56d87d415dbaa681c0a6
nc <charles.saternos@gmail.com>
2023-11-18 14:07:06 -0500

    refactor collision resolution code

--- util.py
+++ util.py
@@ -43,28 +43,37 @@ def camera_follow_window(camera, target_pos, window_width, window_height):
     camera.target.x = (errx + target_pos.x)
     camera.target.y = (erry + target_pos.y)
 
+def copy_rect(rect):
+    return rl.Rectangle(rect.x, rect.y, rect.width, rect.height)
+
+def get_signed_collision_rec(rect1: rl.Rectangle, rect2: rl.Rectangle) -> rl.Rectangle:
+    """Compute the rectangle of intersection between rect1 and rect2.
+
+    If rect2 is to the left or above rect1, the width or height will
+    be flipped, respectively."""
+    r = rl.get_collision_rec(rect1, rect2)
+    if rect2.x < rect1.x:
+        r.width = -r.width
+    if rect2.y < rect1.y:
+        r.height = -r.height
+    return r
+
 def resolve_map_collision(map_aabbs, actor_aabb) -> Optional[glm.vec2]:
-    '''Fix overlap with map tiles. Returns new position for actor_aabb.'''
+    """Fix overlap with map tiles. Returns new position for actor_aabb."""
     # internal copy of actor_aabb that will be mutated
-    aabb = rl.Rectangle(actor_aabb.x, actor_aabb.y,
-                        actor_aabb.width, actor_aabb.height)
-    for i in range(3):
-        overlap = glm.vec2()
-        most_area = 0
-        for i, map_aabb in enumerate(map_aabbs):
-            overlap_rec = rl.get_collision_rec(map_aabb, aabb)
-            overlap_area = overlap_rec.width * overlap_rec.height
-            if overlap_area > most_area:
-                most_area = overlap_area
-                if overlap_rec.width < overlap_rec.height:
-                    dir = -1 if aabb.x < map_aabb.x else 1
-                    overlap = glm.vec2(dir * overlap_rec.width, 0)
-                else:
-                    dir = -1 if aabb.y < map_aabb.y else 1
-                    overlap = glm.vec2(0, dir * overlap_rec.height)
-        aabb.x += overlap.x
-        aabb.y += overlap.y
-    return glm.vec2(aabb.x, aabb.y) if (aabb.x, aabb.y) != (actor_aabb.x, actor_aabb.y) else None
+    aabb = copy_rect(actor_aabb)
+    if map_aabbs:
+        for i in range(3): # run multiple iters to handle corners/passages
+            most_overlap = max((get_signed_collision_rec(r, aabb) for r in map_aabbs),
+                               key=lambda x: abs(x.width * x.height))
+            if abs(most_overlap.width) < abs(most_overlap.height):
+                aabb.x += most_overlap.width
+            else:
+                aabb.y += most_overlap.height
+
+    new_pos = glm.vec2(aabb.x, aabb.y)
+    old_pos = (actor_aabb.x, actor_aabb.y)
+    return new_pos if new_pos != old_pos else None
 
 class CooldownTimer:
     def __init__(self, cooldown):
